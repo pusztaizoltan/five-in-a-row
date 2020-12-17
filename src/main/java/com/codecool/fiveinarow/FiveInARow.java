4924d63fb519aadd9985388152be253191038371
Hullicatantus <nsl@freemail.hu>
2020-12-17 03:46:23 +0100

    add Start Screen, correct call of printResults

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -3,9 +3,12 @@ package com.codecool.fiveinarow;
 public class FiveInARow {
 
     public static void main(String[] args) throws InterruptedException {
+        StartScreen.Gomoku();
         Game game = new Game(19, 19);
-        game.enableAi(1);
+        // game.enableAi(1);
         game.enableAi(2);
         game.play(5);
     }
+
+
 }
--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -1,23 +1,14 @@
 package com.codecool.fiveinarow;
+
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class Game implements GameInterface {
-
     private int[][] board;
     boolean player1AI = false; // added for AI
     boolean player2AI = false; // added for AI
     AI ai; // added for AI
     int howMany; // added for AI
-    // add for colorization
-    private int oldPlayer1ColIndex = 1;
-    private int oldPlayer1RowIndex = 1;
-    private int oldPlayer2ColIndex = 1;
-    private int oldPlayer2RowIndex = 1;
-    private int olderPlayer1ColIndex = 1;
-    private int olderPlayer1RowIndex = 1;
-    private int olderPlayer2ColIndex = 1;
-    private int olderPlayer2RowIndex = 1;
 
     public Game(int nRows, int nCols) {
         this.board = new int[nRows + 1][nCols + 1];
@@ -50,7 +41,7 @@ public class Game implements GameInterface {
             else if (coordinates.length() == 3)
                 move[1] = Character.getNumericValue(coordinates.charAt(1)) * 10 + Character.getNumericValue(coordinates.charAt(2));
 
-            if (move[0] <= maxRow && move[0] >= 0 && move[1] <= maxCol && move[1] > 0) {
+            if (move[0] <= maxRow && move[0] >= 0 && move[1] <= maxCol && move[1] > 0 && isValidMark(move)) {
                 break;
             } else {
                 System.out.println("Try Again!");
@@ -59,26 +50,10 @@ public class Game implements GameInterface {
         return move;
     }
 
-    public int[] getAiMove(int player) {
-        int otherPlayer = (player == 1) ? 2:1;
-        this.ai = new AI(this.board, this.howMany);
-        ai.valueBoard = ai.evaluateBoard(getBoard(), this.howMany, player, otherPlayer);
-        return ai.getCoordinateWithMaxValue(ai.valueBoard);
-    }
 
-    public void mark(int player, int row, int col) {   if (board[row][col] == 0) {
-        if (player == 1){
-            board[row][col] = 1;
-            olderPlayer1ColIndex = col;
-            olderPlayer1RowIndex = row;
-        }
-        if (player == 2) {
-            board[row][col] = 2;
-            olderPlayer2ColIndex = col;
-            olderPlayer1RowIndex = row;
-    }} else {
-        System.out.println("Already marked!");
-    }}
+    public void mark(int player, int row, int col) {
+        board[row][col] = player;
+    }
 
     public boolean hasWon(int player, int howMany) {
         int counter = 0;
@@ -139,51 +114,45 @@ public class Game implements GameInterface {
     }
 
     public void printBoard() {
+        System.out.println();
         int colIndex = 1;
         char rowIndex = 'A';
+
         for (int i = 0; i < board.length; i++) {
+            if (i==1) {
+                System.out.print(ConsoleColors.YELLOW_BRIGHT + "  +");
+                System.out.print(repeat("---", board.length-1));
+                System.out.print("--+" + ConsoleColors.RESET);
+                System.out.println();
+            }
             for (int j = 0; j < board[i].length; j++) {
-                if (i == 0 && j == 0) System.out.print("   ");
+                if (i == 0 && j == 0) System.out.print("     ");
                 else if (i == 0) {
-                    System.out.print(ConsoleColors.CYAN_BRIGHT + colIndex + " " + ConsoleColors.RESET);
+                    System.out.print(ConsoleColors.CYAN_BRIGHT + colIndex + " "+ ConsoleColors.RESET);
                     if (colIndex < 10) System.out.print(" ");
                     colIndex++;
                 } else if (j == 0) {
-                    System.out.print(ConsoleColors.CYAN_BRIGHT + rowIndex + "  " + ConsoleColors.RESET);
+                    System.out.print(ConsoleColors.CYAN_BRIGHT + rowIndex + ConsoleColors.YELLOW_BRIGHT + " | " + ConsoleColors.RESET);
                     rowIndex++;
-                } else if (board[i][j] == 0) System.out.print(".  ");
-                else if (colIndex == olderPlayer1ColIndex && rowIndex == olderPlayer1RowIndex) System.out.print(ConsoleColors.PURPLE + "X  "  + ConsoleColors.RESET);
-                else if (board[i][j] == 1) {
-                    System.out.print(ConsoleColors.RED + "X  " + ConsoleColors.RESET);
-                }
-                else if (colIndex == olderPlayer2ColIndex && rowIndex == olderPlayer2RowIndex) System.out.print(ConsoleColors.YELLOW + "O  " + ConsoleColors.RESET);
-                else if (board[i][j] == 2) System.out.print(ConsoleColors.GREEN_BRIGHT + "O  " + ConsoleColors.RESET);
+                } else if (board[i][j] == 0) System.out.print(" . ");
+                else if (board[i][j] == 1) System.out.print(ConsoleColors.RED_BRIGHT + " X " + ConsoleColors.RESET);
+                else if (board[i][j] == 2) System.out.print(ConsoleColors.GREEN_BRIGHT + " O "+ ConsoleColors.RESET);
             }
-            oldPlayer1ColIndex = olderPlayer1ColIndex;
-            oldPlayer1RowIndex = olderPlayer1RowIndex;
-            oldPlayer2ColIndex = olderPlayer2ColIndex;
-            oldPlayer2RowIndex = olderPlayer2RowIndex;
-
+            if (i != 0)System.out.print(ConsoleColors.YELLOW_BRIGHT + " |");
             System.out.println();
         }
-        try
-        {
-            Thread.sleep(500);
-        }
-        catch(InterruptedException ex)
-        {
-            Thread.currentThread().interrupt();
-        }
+        System.out.print(ConsoleColors.YELLOW_BRIGHT + "  +");
+        System.out.print(repeat("---", board.length-1));
+        System.out.print("--+" + ConsoleColors.RESET);
+        System.out.println();
     }
 
-    public void printResult(int player) {
-            if (player == 1) {
-                System.out.println("'X' won!");
-            } else if (player == 2) {
-                System.out.println("'O' won!");
-            } else {
-                System.out.println("It's a tie!");
-            }
+    public String repeat(String str, int repetition){
+        String result = "";
+        for (int i = 0; i < repetition; i++) {
+            result += str;
+        }
+        return result;
     }
 
     public void enableAi(int player) {
@@ -191,65 +160,89 @@ public class Game implements GameInterface {
         if (player == 2) player2AI = true;
     }
 
-        public void play(int howMany) throws InterruptedException {
-            this.howMany = howMany; // added for AI
-            int[] move;
-
-            while (true) {
-                if (player1AI) { // added for AI
-                    try {
-                        Thread.sleep(600);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    move = getAiMove(1);
-                    mark(1, move[0], move[1]);
-                    ai.printBoard(ai.valueBoard);
-                } else {
-                    printBoard();
-                    move = getMove(1);
-                    mark(1, move[0], move[1]);
-
-                }
-                if (isFull()) {
-                    printBoard();
-                    printResult(0);
-                    break;
-                }
-                if (hasWon(1, howMany)) {
-                    printBoard();
-                    printResult(1);
-                    break;
-                }
-                if (player2AI) { // added for AI
-                    try {
-                        Thread.sleep(600);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    move = getAiMove(2);
-                    mark(2, move[0], move[1]);
-                    ai.printBoard(ai.valueBoard);
-                } else {
-                    printBoard();
-                    move = getMove(2);
-                    mark(2, move[0], move[1]);
-                }
-                if (isFull()) break;
-                if (hasWon(2, howMany)) {
-                    printBoard();
-                    printResult(2);
-                    break;
-                }
-            }
-            System.out.println("Game Over");
+    public int[] getAiMove(int player) {
+        int otherPlayer = (player == 1) ? 2:1;
+        this.ai = new AI(getSubBoard(this.board), this.howMany);
+        ai.valueBoard = ai.evaluateBoard(getSubBoard(this.board), this.howMany, player, otherPlayer);
+        return ai.getCoordinateWithMaxValue(ai.valueBoard);
+    }
+    public int[][] getSubBoard(int[][] board) {
+        int[][] subBoard = new int[board.length-1][board[0].length-1];
+        for (int i = 1; i < board.length; i++) {
+            subBoard[i-1] = Arrays.stream(board[i]).skip(1).toArray();
         }
+        return subBoard;
+    }
 
-    private boolean isEmpty (int row, int col) {
-        if (board[row][col] == 0) {
-            return true;
+    private boolean isValidMark (int[] move){
+        return (this.board[move[0]][move[1]] == 0);
+    }
+
+    public void play(int howMany) {
+        this.howMany = howMany; // added for AI
+        int[] move;
+
+        while (true) {
+
+            if (player1AI) { // added for AI
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                move = getAiMove(1);
+                mark(1, move[0]+1, move[1]+1);
+                ai.printBoard(ai.valueBoard);
+            } else {
+                printBoard();
+                move = getMove(1);
+                mark(1, move[0], move[1]);
+
+            }
+            if (isFull()) {
+                printBoard();
+                printResult(0); // add printresultbreak;
+                break;
+            }
+            if (hasWon(1, howMany)){
+                printBoard();
+                printResult(1); // add printresult
+                break;
+            }
+            if (player2AI) { // added for AI
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                move = getAiMove(2);
+                mark(2, move[0]+1, move[1]+1);
+                ai.printBoard(ai.valueBoard);
+            } else {
+                printBoard();
+                move = getMove(2);
+                mark(2, move[0], move[1]);
+            }
+            if (isFull()) {
+                printBoard();
+                printResult(0); // add printresultbreak;
+                break;
+            }
+            if (hasWon(2, howMany)){
+                printBoard();
+                printResult(2); // add printresult
+                break;}
+        }
+        System.out.println("Game Over");
+    }
+
+    public void printResult(int player) {
+        if (player == 1) {
+            System.out.println(ConsoleColors.RED_BRIGHT + "'X' won!" + ConsoleColors.RESET);
+        } else if (player == 2) {
+            System.out.println(ConsoleColors.GREEN_BRIGHT + "'O' won!" + ConsoleColors.RESET);
         } else {
-            return false;
+            System.out.println(ConsoleColors.YELLOW_BRIGHT + "It's a tie!" + ConsoleColors.RESET);
         }
     }
 }
